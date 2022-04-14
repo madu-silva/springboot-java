388c6c7a18f21b58222da87a24c6027cc053d637
Maria Eduarda <mariaeduarda050501@gmail.com>
2022-04-14 11:52:07 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
